4fcd4a11db8376b19446cf0a3fffda4afef8e746
AriBermeki <ari.bermeki.de@gmail.com>
2026-06-12 19:44:47 +0200

    update v20

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ pub mod utils;
 pub mod webview;
 pub mod window;
 use crate::utils::error::Error;
-
+pub use crate::protocol::{ipc::{IpcBody,IpcRequest,IpcResponse,IpcResponseBody}};
 /// Result type.
 pub type Result<T> = std::result::Result<T, Error>;
--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,4 +1,4 @@
-pub(crate) mod ipc;
+pub mod ipc;
 pub(crate) mod isolation;
 pub(crate) mod pattern;
 pub(crate) mod taurino;
